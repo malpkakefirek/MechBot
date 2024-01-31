92d03e3192687604895328678f413cc137fc4121
malpkakefirek <malpkakefirek@gmail.com>
2024-01-31 23:15:18 +0100

    fix int json parsing

--- handle_database.py
+++ handle_database.py
@@ -5,7 +5,7 @@ import json
 async def select_value(cursor, key):
     await cursor.execute("SELECT value FROM mechy WHERE key = ?", (key, ))
     row = await cursor.fetchone()
-    return json.loads(row[0])
+    return json.loads(str(row[0]))
 
 async def update_value(cursor, key, value):
     value = json.dumps(value)
@@ -14,7 +14,7 @@ async def update_value(cursor, key, value):
 
 def select_value_sync(cursor, key):
     cursor.execute("SELECT value FROM mechy WHERE key = ?", (key, ))
-    return json.loads(cursor.fetchone()[0])
+    return json.loads(str(cursor.fetchone()[0]))
 
 def update_value_sync(cursor, key, value):
     value = json.dumps(value)
